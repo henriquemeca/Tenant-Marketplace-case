a4698500916138dae19ba00cfd439b1cbdf5554b
312 <3213@eewq.com>
2022-05-30 01:03:41 -0300

    Enough data for finishing project

--- Analisys/dataTools.py
+++ Analisys/dataTools.py
@@ -1,5 +1,6 @@
 import imp
 from importlib.resources import path
+from xml.etree.ElementInclude import include
 import pandas as pd
 from os import listdir
 import json 
@@ -18,8 +19,44 @@ def getJsons(folder)->list:
             files[path.replace(folder,'').replace('.json','')] = f
     return files
 
-class dataExplorer: 
-    def __init__(self,data) -> None:
-        if not isinstance(data, pd.DataFrame):
-            raise ValueError('Insert a pandas DataFrame')
-        self.data=data
+def explore(data):
+    print('----Data Sample----')
+    print(data.head())
+    print('----About the columns----')
+    print(data.info())
+    print('----Unique values in columns----')
+    print(data.nunique())
+    print('----Numeric columns statistics----')
+    print(data.describe())
+    print('')
+
+def plots(data):
+    print('----Columns Histogram----')
+    """
+        function1: general plots
+            correlations(heatmap) ,Cluster
+        function2: column analyser
+            histogram, boxplot,valuecounts pie, violin plot
+        function3: specific plots 2 variables
+            Pareto, Ogiva, density(with mean and meadian on the chart), scatter
+        function4: plot grids
+            seaborn scater plot grids / pairplot / relplot / distplot / cat plot
+
+        CRISP DM method
+        - Barry | Business Understading
+        - Drove | Data Understanding
+        - Directly to the | Data Prep
+        - Medical | Modeling
+        - Emergency | Evaluation
+        - Department | Deployment
+    """
+    
+    # for (columnName, columnData) in data.iteritems():
+    #     graph = data[columnName].plot
+
+        
+# class dataExplorer: 
+#     def __init__(self,data) -> None:
+#         if not isinstance(data, pd.DataFrame):
+#             raise ValueError('Insert a pandas DataFrame')
+#         self.data=data
